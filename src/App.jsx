62477629c2e9604769280f22c53a99aa7834c29a
Assel <asselaltayeva@users.noreply.github.com>
2025-06-08 22:09:24 +0900

    Footed added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import Page from "./components/sections/Page";
 
+//Header
 import Header from "./components/sections/Header";
 import Navigation from "./components/sections/Navigation";
 import Hero from "./components/sections/Hero";
 import Reviews from "./components/sections/Reviews";
 
+//Main
 import Main from "./components/sections/Main";
 import Logos from "./components/sections/Logos";
 import Features from "./components/sections/Features";
 import FAQs from "./components/sections/FAQs/FAQs";
 import Testimonials from "./components/sections/Testimonials/Testimonials";
+
+//Footer
+import Footer from "./components/sections/Footer";
 
 function App() {
   return ( 
@@ -26,6 +31,8 @@ function App() {
       <FAQs />
       <Testimonials />
     </Main>
+
+    <Footer />
   </Page>
   );
 }
